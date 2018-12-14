9ade2012585b2587ac61bb69b457239337703c6c
Eugene Clewlow <eclewlow@Eugenes-MacBook-Pro.local>
2018-12-14 11:41:51 -0800

    installs latest tagged version of Flutter from flutter/flutter git repository

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # brew-formulas
+
+## Flutter
+flutter-sdk path will be /usr/local/opt/flutter
+dart-sdk path will be /usr/local/opt/flutter/bin/cache/dart-sdk
+
+You still need to install Xcode and Android emulator, etc.
+https://flutter.io/docs/get-started/install/macos
+
+And create your project pubspec, etc.
+https://www.dartlang.org/tools/pub/pubspec
--- flutter.rb
+++ flutter.rb
@@ -1,14 +1,27 @@
 class Flutter < Formula
   desc "Build beautiful native apps in record time"
   homepage "https://flutter.io/"
-  url "https://storage.googleapis.com/flutter_infra/releases/beta/macos/flutter_macos_v0.7.3-beta.zip"
-  sha256 "72ee026522be9cfc552b02886c9697f8bee0626635bc425b41b64c01e5253565"
+  version "1.1.1"
+  url "https://github.com/flutter/flutter.git"
+  sha256 "6e3ba2da94fb150d2cb8e0596eeb92c86e5e437e91a5f39f211bd4b21d0b24f8"
 
   bottle :unneeded
 
   def install
+    system "git", "fetch", "--tags"
+
+    # checkout latest tag
+    latest_tag = `git tag | tail -1`
+    puts latest_tag
+    system "git checkout tags/#{latest_tag}"
+
+    system "./bin/flutter"
+
     allfiles = File.join(buildpath, "**", "{*,.*}")
     mv Dir.glob(allfiles), Dir.glob(prefix), :force => true
+
+    # bin.install File.join(prefix, "bin/cache/dart-sdk/bin/dart")
+    # bin.install File.join(prefix, "bin/cache/dart-sdk/bin/pub")
   end
 
   def post_install
