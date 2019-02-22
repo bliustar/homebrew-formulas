f171cb59c221661a7f66da343aed2bef52d0c2fa
bliustar <bliustar@gmail.com>
2019-02-22 17:01:24 +0900

    1.2.2

--- flutter.rb
+++ flutter.rb
@@ -1,12 +1,14 @@
 class Flutter < Formula
   desc "Build beautiful native apps in record time"
   homepage "https://flutter.io/"
-  version "1.0.0"
-  url "https://github.com/flutter/flutter.git", :branch => "stable"
+  version "1.2.2"
+  url "https://github.com/flutter/flutter.git"
+  sha256 "6e3ba2da94fb150d2cb8e0596eeb92c86e5e437e91a5f39f211bd4b21d0b24f8"
+
   bottle :unneeded
 
   def install
-    system "git", "fetch", "--all", "--prune", "--tags"
+    system "git", "fetch", "--tags"
 
     # checkout latest tag
     latest_tag = `git tag | tail -1`
