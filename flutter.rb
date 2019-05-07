f8134d769c82431e7e3d76de9a20bb1eab73d6fd
bliustar <bliustar@gmail.com>
2019-05-08 08:14:38 +0900

    v1.5.8

--- flutter.rb
+++ flutter.rb
@@ -1,7 +1,7 @@
 class Flutter < Formula
   desc "Build beautiful native apps in record time"
   homepage "https://flutter.io/"
-  version "1.2.2"
+  version "1.5.8"
   url "https://github.com/flutter/flutter.git"
   sha256 "6e3ba2da94fb150d2cb8e0596eeb92c86e5e437e91a5f39f211bd4b21d0b24f8"
 
