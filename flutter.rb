56c84d055e8ae68370933652f7f5a07977da3420
bliustar <bliustar@gmail.com>
2019-07-16 17:01:39 +0900

    1.8.1

--- flutter.rb
+++ flutter.rb
@@ -1,9 +1,9 @@
 class Flutter < Formula
   desc "Build beautiful native apps in record time"
   homepage "https://flutter.io/"
-  version "1.5.8"
+  version "1.8.1"
   url "https://github.com/flutter/flutter.git"
-  sha256 "6e3ba2da94fb150d2cb8e0596eeb92c86e5e437e91a5f39f211bd4b21d0b24f8"
+  sha256 "d3eee57c0bd2e19aaa944be07b24c533075fd1a0"
 
   bottle :unneeded
 
